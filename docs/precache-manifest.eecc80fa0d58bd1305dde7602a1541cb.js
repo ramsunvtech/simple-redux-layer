1294ad48b8c40d9d1ec97c159c2d914e20d484c8
Venkatraman <ramsunvtech@gmail.com>
2020-06-20 16:41:29 +0800

    chore(app): Update Initial Test Build App

---
+++ docs/precache-manifest.eecc80fa0d58bd1305dde7602a1541cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aedfb7dde4e08c69dd9030610a0274a7",
+    "url": "./simple-redux-layer/index.html"
+  },
+  {
+    "revision": "e766e1636429c6bcf9e1",
+    "url": "./simple-redux-layer/static/js/2.20c999c5.chunk.js"
+  },
+  {
+    "revision": "7e402396ca428a5bd514c48df615b24e",
+    "url": "./simple-redux-layer/static/js/2.20c999c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e189b42e0f3e04faa22b",
+    "url": "./simple-redux-layer/static/js/main.9a197359.chunk.js"
+  },
+  {
+    "revision": "457e8ddca27513c3ce46",
+    "url": "./simple-redux-layer/static/js/runtime-main.6faeaf8b.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.edd764fd193eaf3c9e716c2217089662.js"
+  "./simple-redux-layer/precache-manifest.eecc80fa0d58bd1305dde7602a1541cb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./simple-redux-layer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
