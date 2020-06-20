b1f30bb92d0a69164adbf9a7f43dd1e3d6c308a0
Venkatraman <ramsunvtech@gmail.com>
2020-06-20 21:29:28 +0800

    feat(header): Updated AppBar in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
 
 const Header = () => (
-  <ul>
-    <li>
-      <Link to="/">Home</Link>
-    </li>
-    <li>
-      <Link to="/create">Create</Link>
-    </li>
-  </ul>
+  <Fragment>
+    <AppBar>
+      <Toolbar>
+      <Typography variant="h6">Simple Redux Layer</Typography>&nbsp; - &nbsp;
+        <Link to="/" style={{color: 'white', textDecoration: 'none' }}>
+          <Typography variant="h6">Home</Typography>
+        </Link>
+        &nbsp; | &nbsp; 
+        <Link to="/create" style={{color: 'white', textDecoration: 'none' }}>
+          <Typography variant="h6">Create Part</Typography>
+        </Link>
+      </Toolbar>
+    </AppBar>
+  </Fragment>
 );
 
 export default Header;
