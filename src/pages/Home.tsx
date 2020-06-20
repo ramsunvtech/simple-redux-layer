bc879a425e4380fad2557ae1b020fb2771df8776
Venkatraman <ramsunvtech@gmail.com>
2020-06-20 21:32:42 +0800

    feat(home): Updated Part Component with Try Block

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Part from '../components/Part';
 
+// `Material` Components.
+import Grid from '@material-ui/core/Grid';
+
+// Stylesheet.
+import useStyles from './Home.style';
+
 const Home = () => {
+  const classes = useStyles();
+  let items: array = [];
+
+  try {
+    const parts = JSON.parse(window.sessionStorage.getItem('parts'));
+    items = parts.items || [];
+  } catch (e) {
+
+  }
+  const [partItemsList] = useState(items);
+
   return (
-    <div>
-      These parts are hard-coded; they should be removed.
-      <Part name="Test part" id={123} status="Checked In" />
-      <Part name="Another part" id={456} status="Checked Out" />
-    </div>
+    <Grid container>
+      {partItemsList.map((partItem: object) => {
+        return (
+          <Grid item xl={3} className={classes.item}>
+            <Part name={partItem.name} id={partItem.id} status={partItem.status} />
+          </Grid>
+        );
+      })}
+    </Grid>
   );
 };
 
