1f9cd8d71e4b2f54c0a355453cc53d36415ec831
Venkatraman <ramsunvtech@gmail.com>
2020-06-20 21:33:40 +0800

    style(home): Updated Part List Style

---
+++ src/components/Part.style.ts
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
