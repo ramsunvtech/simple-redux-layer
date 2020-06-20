e82bf714cb0f84bee650d9c5f3739d744bd8cf7b
Venkatraman <ramsunvtech@gmail.com>
2020-06-20 21:35:08 +0800

    style(base): Added Custom Material UI theme

---
+++ src/theme/breakPoints.ts
@@ -0,0 +1,10 @@
+
+export default {
+  values: {
+    xs: 0,
+    sm: 360,
+    md: 600,
+    lg: 1024,
+    xl: 1280,
+  }
+};
---
+++ src/theme/index.ts
@@ -0,0 +1,19 @@
+import { createMuiTheme } from '@material-ui/core';
+
+import palette from './palette';
+import breakpoints from './breakPoints';
+import typography from './typography';
+import overrides from './overrides';
+
+const theme = createMuiTheme({
+  breakpoints,
+  palette,
+  typography,
+  overrides,
+  zIndex: {
+    appBar: 1200,
+    drawer: 1100
+  }
+});
+
+export default theme;
