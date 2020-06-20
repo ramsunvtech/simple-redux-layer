6b5c9f6efe23eebbdb48ce805b402ff38c990c5e
Venkatraman <ramsunvtech@gmail.com>
2020-06-20 21:33:14 +0800

    tech(customStore): Added Custom Store Test Code

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/styles';
+
+// `Material` Components.
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+
+// `Local` Components.
 import CreatePart from './pages/CreatePart';
 import Home from './pages/Home';
 import Header from './components/Header';
+import theme from './theme/index';
+
+// Store Lib.
+import { createStore } from './lib/index';
 
 function App() {
+
+  useEffect(() => {
+    console.log('create store')
+    createStore();
+  }, []);
+
   return (
     <Router>
-      <div className="App">
-        <Header />
-        <div>
-          <Route exact path="/" component={Home} />
-          <Route path="/create" component={CreatePart} />
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Header />
+          <Container>
+            <Box my={10}>
+              <Route exact path="/" component={Home} />
+              <Route path="/create" component={CreatePart} />
+            </Box>
+          </Container>
         </div>
-      </div>
+      </ThemeProvider>
     </Router>
   );
 }
