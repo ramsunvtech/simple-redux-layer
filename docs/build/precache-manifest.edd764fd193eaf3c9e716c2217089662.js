9858cd83118aa02d732890d6a00b018cad0fd0d7
Venkatraman <ramsunvtech@gmail.com>
2020-06-20 16:38:33 +0800

    chore(app): Update Initial Test Build App

---
+++ docs/build/precache-manifest.edd764fd193eaf3c9e716c2217089662.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dbd94e5cb4f3b1c7ad08312ee794b58",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e766e1636429c6bcf9e1",
+    "url": "./static/js/2.20c999c5.chunk.js"
+  },
+  {
+    "revision": "7e402396ca428a5bd514c48df615b24e",
+    "url": "./static/js/2.20c999c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e189b42e0f3e04faa22b",
+    "url": "./static/js/main.9a197359.chunk.js"
+  },
+  {
+    "revision": "eca12fc47ea2210fd06a",
+    "url": "./static/js/runtime-main.6111cb5d.js"
+  }
+]);
