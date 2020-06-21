3e85bb42942302901e51a6d01f162d11a8581aba
Venkatraman <ramsunvtech@gmail.com>
2020-06-21 09:38:49 +0800

    feat(home): Updated Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Part from '../components/Part';
 
 // `Material` Components.
@@ -7,21 +7,34 @@ import Grid from '@material-ui/core/Grid';
 // Stylesheet.
 import useStyles from './Home.style';
 
+
+// Tiny Redux.
+import { getStore } from '../lib/customRedux';
+// import { tinyConnect } from '../lib/customReactRedux';
+
+const tinyConnect = (Component): React.FC => ({
+  loading,
+  ...props
+}) => {
+  return (
+    <Component {...props as P} />
+  );
+}
+
 const Home = () => {
   const classes = useStyles();
-  let items: array = [];
+  const [partItemList, setPartItemList] = useState([]);
 
-  try {
-    const parts = JSON.parse(window.sessionStorage.getItem('parts'));
-    items = parts.items || [];
-  } catch (e) {
+  useEffect(() => {
+    const { parts = {} } = getStore();
+    const { items = [] } = parts;
 
-  }
-  const [partItemsList] = useState(items);
+    setPartItemList(items);
+  }, [window.appStore]);
 
   return (
     <Grid container>
-      {partItemsList.map((partItem: object) => {
+      {partItemList.map((partItem: object) => {
         return (
           <Grid item xl={3} className={classes.item}>
             <Part name={partItem.name} id={partItem.id} status={partItem.status} />
@@ -32,4 +45,19 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default tinyConnect(Home);
+
+const mapStateToProps = (state) => {
+  return {
+    parts: state.parts,
+  };
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    // dispatching plain actions
+    loadParts: () => dispatch({ type: 'LOAD_PARTS' }),
+  }
+}
+
+// export default tinyConnect(Home);
