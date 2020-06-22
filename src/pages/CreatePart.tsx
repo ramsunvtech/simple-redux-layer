e27b51f765492ac3971ee17927c65fc36c495cd3
Venkatraman <ramsunvtech@gmail.com>
2020-06-22 23:06:43 +0800

    feat(createPart): Updated Typings and Responsiveness for Form

--- src/pages/CreatePart.tsx
+++ src/pages/CreatePart.tsx
@@ -5,6 +5,9 @@ import { v4 as uuidv4 } from 'uuid';
 import { useHistory } from "react-router-dom";
 
 // `Material` Components.
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import TextField from '@material-ui/core/TextField';
 import Switch from '@material-ui/core/Switch';
 import Button from '@material-ui/core/Button';
@@ -15,7 +18,21 @@ import { tinyConnect } from '../lib/tinyReactRedux';
 // `Actions`.
 import * as actionTypes from '../store/actions/actionTypes';
 
-const CreatePart = ({ createPart }) => {
+// Stylesheet.
+import useStyles from './CreatePart.style';
+
+interface Part {
+  name: string;
+  id: number;
+  status: boolean;
+};
+
+interface CreatePartPageProps {
+  createPart: (part: Part) => void;
+};
+
+const CreatePart: React.FC<CreatePartPageProps> = ({ createPart }) => {
+  const classes = useStyles();
   const history = useHistory();
   const [checkStatus, setCheckStatus] = useState(true);
   const [partName, setPartName] = useState('');
@@ -23,14 +40,14 @@ const CreatePart = ({ createPart }) => {
   const handleSwitch = (event: any) => {
     setCheckStatus(!checkStatus);
   };
-  
+
   const handleNameChange = (event: any) => {
     setPartName(event.target.value);
   };
 
   const onSubmit = (e: any) => {
     e.preventDefault();
- 
+
     createPart({
       name: partName, id: uuidv4(), status: checkStatus
     });
@@ -39,30 +56,60 @@ const CreatePart = ({ createPart }) => {
 
   return (
     <form noValidate autoComplete="off" onSubmit={onSubmit}>
-      <TextField id="name" label="Name" value={partName} InputProps={{ style: { color: 'black' } }} color="secondary" onChange={handleNameChange} />
-      <Switch
-        checked={checkStatus}
-        onChange={handleSwitch}
-        color="primary"
-        name="switch"
-        inputProps={{ 'aria-label': 'primary checkbox' }}
-      />
-      <Button type="submit" variant="contained" color="primary">
-        Create Part
-      </Button>
+      <Grid container>
+        <Grid item xl={12}>
+          <Typography variant="h4" gutterBottom>
+            Create Part
+          </Typography>
+        </Grid>
+        <Grid item xl={5} sm={12} className={classes.nameField}>
+          <TextField
+            id="name"
+            label="Name"
+            value={partName}
+            InputProps={{
+              style: {
+                color: 'black'
+              }
+            }}
+            color="secondary"
+            onChange={handleNameChange}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xl={12} sm={12} className={classes.statusField}>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={checkStatus}
+                onChange={handleSwitch}
+                color="primary"
+                name="switch"
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              />
+            }
+            label="Status"
+          />
+        </Grid>
+        <Grid item xl={3} sm={12}>
+          <Button type="submit" variant="contained" color="primary" fullWidth>
+            Create Part
+          </Button>
+        </Grid>
+      </Grid>
     </form>
   );
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state: any) => {
   return {
     parts: state.parts.items,
   };
 }
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch: Function) => {
   return {
-    createPart: (partItem) => dispatch({ type: actionTypes.CREATE_PART, partItem }),
+    createPart: (partItem: Part) => dispatch({ type: actionTypes.CREATE_PART, partItem }),
   }
 }
 
