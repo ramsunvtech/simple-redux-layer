93103f34c77ec5970fa8556024d9613cf1f9f24f
Venkatraman <ramsunvtech@gmail.com>
2020-06-23 13:27:11 +0800

    feat(store): Update Store Index

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,12 +3,17 @@ import { initStore, getStore, dispatch } from '../lib/tinyRedux';
 // Reducers.
 import { parts } from './reducers';
 
+// `Models`.
+import { Options } from '../models/Store';
+
 const initializeStore = () => {
-  initStore({
+  const store: Options = {
     reducers: {
       parts,
     },
-  });
+  };
+
+  initStore(store);
 };
 
 export default {
