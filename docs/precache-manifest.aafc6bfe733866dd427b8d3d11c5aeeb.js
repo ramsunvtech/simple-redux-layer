3840c1fabf11358764185d4af947ddb452c54fef
Venkatraman <ramsunvtech@gmail.com>
2020-06-23 13:28:42 +0800

    docs(main): Update Asset Manifest

---
+++ docs/precache-manifest.aafc6bfe733866dd427b8d3d11c5aeeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc31c72f1d934d5e20c45ab45358d53a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2231cedd085e31e6d6af",
+    "url": "./static/js/2.5fa54aea.chunk.js"
+  },
+  {
+    "revision": "7e402396ca428a5bd514c48df615b24e",
+    "url": "./static/js/2.5fa54aea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83a90a931a3ad25a335",
+    "url": "./static/js/main.223b24f5.chunk.js"
+  },
+  {
+    "revision": "eca12fc47ea2210fd06a",
+    "url": "./static/js/runtime-main.6111cb5d.js"
+  }
+]);
