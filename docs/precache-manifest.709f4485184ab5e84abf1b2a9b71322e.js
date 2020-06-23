0941318af8fc70899a8d62e1e5edafad6462fe9a
Venkatraman <ramsunvtech@gmail.com>
2020-06-23 13:51:55 +0800

    docs(main): Update PreCache

---
+++ docs/precache-manifest.709f4485184ab5e84abf1b2a9b71322e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbcb0aa8d167991cec695c7106946ca5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "85ab0f46798efa47bcb4",
+    "url": "./static/js/2.a1f608cd.chunk.js"
+  },
+  {
+    "revision": "7e402396ca428a5bd514c48df615b24e",
+    "url": "./static/js/2.a1f608cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cec2c49e1fc3695c85c",
+    "url": "./static/js/main.26d97559.chunk.js"
+  },
+  {
+    "revision": "eca12fc47ea2210fd06a",
+    "url": "./static/js/runtime-main.6111cb5d.js"
+  }
+]);
