6890cea0116c352bab03b09f1c4189e266f10a5e
Venkatraman <ramsunvtech@gmail.com>
2020-06-23 13:22:59 +0800

    style(createPart): Update Create Part

---
+++ src/pages/CreatePart.style.ts
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(theme => ({
+  nameField: {
+    margin: theme.spacing(0, 0, 2, 0),
+  },
+  statusField: {
+    margin: theme.spacing(0, 0, 2, 0),
+  },
+}));
