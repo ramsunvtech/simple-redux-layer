cfaabb8fa8263b26eb6aa815792bbb052b1fa0a0
Venkatraman <ramsunvtech@gmail.com>
2020-06-24 00:18:57 +0800

    test(createPart): Add Create Part Test

---
+++ src/pages/CreatePart.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  render,
+  cleanup,
+} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+// `Test` Component.
+import CreatePart from './CreatePart';
+
+jest.mock('../lib/tinyReactRedux', () => {
+  return {
+    tinyConnect: (a, b) => (Component) => {
+      return <Component createPart={() => {}} />
+    },
+  };
+});
+
+describe('CreatePart', () => {
+  afterEach(cleanup);
+
+  it('should take a snapshot', () => {
+    const { asFragment } = render(<CreatePart />);
+    expect(asFragment(<CreatePart />)).toMatchSnapshot();
+  });
+});
