17f3d69b631f1f1cda95466ef76bde3acc2c7d3f
Venkatraman <ramsunvtech@gmail.com>
2020-06-23 13:27:57 +0800

    style(home): Update Home Page

--- src/pages/Home.style.ts
+++ src/pages/Home.style.ts
@@ -1,7 +1,13 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(theme => ({
-    item: {
-        margin: theme.spacing(2, 5),
+  item: {
+    margin: theme.spacing(2, 5),
+    [theme.breakpoints.down('md')]: {
+      margin: theme.spacing(2),
     },
+    [theme.breakpoints.down('sm')]: {
+      margin: theme.spacing(2),
+    },
+  },
 }));
